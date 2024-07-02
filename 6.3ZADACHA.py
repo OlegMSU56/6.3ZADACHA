d5114b347a25d7f35138a3db1033a7913ff2562d
Oleg <sagdeev.ol@yandex.ru>
2024-07-02 10:42:28 +0300

    Update 6.3ZADACHA.py

--- 6.3ZADACHA.py
+++ 6.3ZADACHA.py
@@ -1,18 +1,17 @@
 class Horse:
-    def __init__(self, x_distance, y_distance, sound='Frrr'):
-        self.x_distance = x_distance
-        self.sound = sound
-        super().__init__(y_distance)
-
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+        super().__init__()
     def run(self, dx):
         self.x_distance += dx
         return self.x_distance
 
 
 class Eagle:
-    def __init__(self, y_distance, sound='I train, eat, sleep, and repeat'):
-        self.y_distance = y_distance
-        self.sound = sound
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.y_distance += dy
@@ -20,23 +19,20 @@ class Eagle:
 
 
 class Pegas(Horse, Eagle):
-    def __init__(self, x_distance, y_distance):
-        super().__init__(x_distance, y_distance)
-
-
+    def __init__(self):
+        super().__init__()
     def move(self, dx, dy):
-        return (self.run(dx),self.fly(dy))
-
+        return (self.run(dx), self.fly(dy))
 
     def get_pos(self):
         self.pos = (self.x_distance, self.y_distance)
         return self.pos
 
     def voice(self):
-        print(self.sound)#Почему у нас sound берется от Eagle, если область видимости Horse 'нам ближе' по mro?
+        print(self.sound)
 
 
-p1 = Pegas(0, 0)
+p1 = Pegas()
 
 print(p1.get_pos())
 p1.move(10, 15)
